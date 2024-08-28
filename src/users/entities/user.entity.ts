8d3433672a7910e0651c744231a6f8bef3904624
ViniciusKuchnir <vina200902@gmail.com>
2024-08-28 14:35:39 -0300

    feat: Create users entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,53 @@
-export class User {}
+import { UserTypeSeed } from 'src/user-type-seed/entities/user-type-seed.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 60, nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', length: 60, nullable: false, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  password: string;
+
+  @CreateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    precision: 0,
+    nullable: false,
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    precision: 0,
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'datetime', nullable: true })
+  deletedAt: Date;
+
+  @ManyToOne(() => UserTypeSeed, (type) => type, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+    nullable: false,
+  })
+  @JoinColumn({ name: 'user_type_id' })
+  user_type: UserTypeSeed;
+}
