e19c2101b0628f8a32955ebbee122eae26dac808
ViniciusKuchnir <vina200902@gmail.com>
2024-08-28 13:53:57 -0300

    feat: Create user_type entity

--- src/user-type-seed/entities/user-type-seed.entity.ts
+++ src/user-type-seed/entities/user-type-seed.entity.ts
@@ -1,1 +1,10 @@
-export class UserTypeSeed {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'user_types' })
+export class UserTypeSeed {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar', length: 15, nullable: false, unique: true })
+  name: string;
+}
