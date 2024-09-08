e62aee1a468f1c24e86a429dbee17bc68e47016c
ViniciusKuchnir <vina200902@gmail.com>
2024-09-08 15:36:41 -0300

    feat: Create product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,64 @@
-export class Product {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Category } from '../../categories/entities/category.entity';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Entity({ name: 'products' })
+export class Product {
+  @PrimaryColumn({ type: 'varchar', length: 36 })
+  sku: string;
+
+  @Column({ type: 'varchar', length: 30, nullable: false, unique: true })
+  name: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  description: string;
+
+  @Column({ type: 'int', nullable: false })
+  quantity: number;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2, nullable: false })
+  unity_price: number;
+
+  @Column({ type: 'boolean', nullable: false, default: true })
+  isActive: boolean;
+
+  @ManyToOne(() => Category, (category) => category, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+    nullable: false,
+  })
+  @JoinColumn({ name: 'category_id' })
+  @ApiProperty({ type: () => Category })
+  category: Category;
+
+  @CreateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    precision: 0,
+    nullable: false,
+  })
+  @ApiProperty({ example: '2024-08-28T18:08:01.000Z' })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    precision: 0,
+  })
+  @ApiProperty({ example: '2024-08-28T18:08:01.000Z' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: 'datetime', nullable: true })
+  @ApiProperty({ example: ['2024-08-28T18:08:01.000Z', null] })
+  deletedAt: Date;
+}
