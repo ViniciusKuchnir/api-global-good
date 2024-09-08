cd32bfcbc69c7e8174e9ffe0cead8ee12e9b9e9d
ViniciusKuchnir <vina200902@gmail.com>
2024-09-08 14:48:09 -0300

    feat: Create category DTO

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,9 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(30, { message: 'Name is too long' })
+  @MinLength(1, { message: 'Name is required' })
+  name: string;
+}
