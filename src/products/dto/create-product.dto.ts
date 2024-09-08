cbe3a2592962c91aa887b3ba2effe0bd272b7d32
ViniciusKuchnir <vina200902@gmail.com>
2024-09-08 16:01:24 -0300

    feat: Create product DTO

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,43 @@
-export class CreateProductDto {}
+import {
+  IsBoolean,
+  IsDecimal,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUUID,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1, { message: 'Name is required' })
+  @MaxLength(30, { message: 'Name is too long' })
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1, { message: 'Description is required' })
+  @MaxLength(255, { message: 'Description is too long' })
+  description: string;
+
+  @IsNumber()
+  @IsInt()
+  @Min(1, { message: 'Quantity must be greater than zero' })
+  quantity: number;
+
+  @IsDecimal()
+  @Min(0.01, { message: 'Unity price must be greater than zero' })
+  unity_price: number;
+
+  @IsBoolean()
+  isActive: boolean;
+
+  @IsString()
+  @IsUUID()
+  @IsNotEmpty()
+  category_id: string;
+}
